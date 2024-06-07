b278a1a11e7556601c826d04a195d60acdcb8cc3
artemijfridriksen <freshart666@gmail.com>
2024-06-07 16:19:39 -0400

    countries & years

---
+++ frontend_admin/lib/api/countries.dart
@@ -0,0 +1,36 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:nimbus/api/constants.dart';
+
+class CountriesResponse {
+  final List<String> countries;
+
+  CountriesResponse({required this.countries});
+
+  factory CountriesResponse.fromJson(Map<String, dynamic> json) {
+    return CountriesResponse(
+      countries: List<String>.from(json['countries']),
+    );
+  }
+}
+
+Future<List<String>?> getCountries({int? year}) async {
+  String rootUrl = '${baseUrl}api/countries';
+  Map<String, String> queryParams = {};
+
+  if (year != null) {
+    queryParams['year'] = year.toString();
+  }
+
+  String queryString = Uri(queryParameters: queryParams).query;
+  String url = queryString.isNotEmpty ? '$rootUrl?$queryString' : rootUrl;
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    Map<String, dynamic> jsonResponse = json.decode(response.body);
+    CountriesResponse countriesResponse = CountriesResponse.fromJson(jsonResponse);
+    return countriesResponse.countries;
+  } else {
+    return null;
+  }
+}
---
+++ frontend_admin/lib/api/years.dart
@@ -0,0 +1,28 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:nimbus/api/constants.dart';
+
+class Years {
+  final List<String> years;
+
+  Years({required this.years});
+
+  factory Years.fromJson(Map<String, dynamic> json) {
+    return Years(
+      years: List<String>.from(json['years']),
+    );
+  }
+}
+
+Future<List<String>?> getYears() async {
+  final response = await http.get(Uri.parse(baseUrl));
+
+  if (response.statusCode == 200) {
+    Map<String, dynamic> jsonResponse = json.decode(response.body);
+    Years yearsResponse = Years.fromJson(jsonResponse);
+    return yearsResponse.years;
+  } else {
+    return null;
+  }
+}
+

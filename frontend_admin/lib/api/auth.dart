4dece3dbaf25ec60492cc7e3083ec52bf70d0edb
Eduard <ed.kechedzhiev@gmail.com>
2024-06-13 19:22:24 -0400

    switched to dio

--- frontend_admin/lib/api/auth.dart
+++ frontend_admin/lib/api/auth.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:crypto/crypto.dart';
+import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 import 'package:nimbus/api/constants.dart';
 
@@ -12,14 +13,19 @@ String _hashPassword(String password) {
 /// it also encodes the pass
 /// true if sucsess
 Future<bool> auth(String email, String pass) async {
-  pass = _hashPassword(pass);
-  final resp = await http.post(
-    Uri.parse("$baseUrl/auth"),
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(<String, String>{"email": email, "password": pass}),
-  );
+  // pass = _hashPassword(pass);
+  final resp;
+  try {
+    resp = await Dio().post("http://127.0.0.1:8000/admin/auth", data: <String, String>{
+      "email": email,
+      "password": pass,
+    });
+  } catch (e) {
+    print("Cought error: $e");
+    return false;
+  }
+
+  print("Code: ${resp.statusCode}|Body: ${resp.data}");
 
   if (resp.statusCode == 200) {
     return true;
@@ -28,18 +34,14 @@ Future<bool> auth(String email, String pass) async {
   }
 }
 
-enum RegisterStatus {
-  Created,
-  AlreadyExists,
-  InternalServerError
-}
+enum RegisterStatus { Created, AlreadyExists, InternalServerError }
 
 Future<RegisterStatus> registerAdmin(
     {required String email,
     required String pass,
     String? name,
     String? lastName}) async {
-  pass = _hashPassword(pass);
+  // pass = _hashPassword(pass);
   final resp = await http.post(
     Uri.parse("$baseUrl/api/register-admin"),
     headers: <String, String>{
@@ -52,16 +54,18 @@ Future<RegisterStatus> registerAdmin(
       "last_name": lastName,
     }),
   );
-  
-  if (resp.statusCode == 201) { // CREATED
+
+  if (resp.statusCode == 201) {
+    // CREATED
+    print("Registered(Email=$email|password=$pass)");
     return RegisterStatus.Created;
-  } else if (resp.statusCode == 409) { // Duplicate
+  } else if (resp.statusCode == 409) {
+    // Duplicate
     return RegisterStatus.AlreadyExists;
   } else {
     return RegisterStatus.InternalServerError;
   }
 }
-
 
 /// Theoretically there is no point of failure
 Future<String> issueVisitorLink({required int validFor}) async {
@@ -81,6 +85,7 @@ Future<String> issueVisitorLink({required int validFor}) async {
     final uuid = jsonResponse["uuid"];
     return "$baseUrl/visitor/$uuid";
   } else {
-    throw Exception('Failed to generate link. Status code: ${response.statusCode}, Body: ${response.body}');
+    throw Exception(
+        'Failed to generate link. Status code: ${response.statusCode}, Body: ${response.body}');
   }
 }

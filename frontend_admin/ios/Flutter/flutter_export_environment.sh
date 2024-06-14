aa39115923afe1dc38df5d410e17f7630f130a97
artemijfridriksen <freshart666@gmail.com>
2024-06-13 21:31:07 -0400

    staged

---
+++ frontend_admin/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/artemijfridriksen/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/artemijfridriksen/projects/portfolio_builder/portfolio_volodymyr_constructor_company/frontend_admin"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"

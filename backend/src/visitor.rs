e18086367cedbce41b69e06e2586260ab658ab8a
Eduard <ed.kechedzhiev@gmail.com>
2024-06-15 18:34:16 -0400

    tmp2

--- backend/src/visitor.rs
+++ backend/src/visitor.rs
@@ -7,34 +7,31 @@ use axum::{
     response::{Html, IntoResponse},
     Json,
 };
-use axum_extra::{extract::cookie::{Cookie, CookieJar}, headers::{authorization::Bearer, Authorization}, TypedHeader};
+use axum_extra::extract::cookie::{Cookie, CookieJar};
 use serde_json::json;
 use tower_http::services::ServeDir;
 const VISITOR_UUID_COOKIE_NAME: &'static str = "visitor-uuid";
 
-pub async fn validate_visitor(
-    bearer: Option<TypedHeader<Authorization<Bearer>>>,
+pub async fn validate_visitor_cookie(
     State(state): State<AppState>,
+    cookie_jar: CookieJar,
     req: Request<Body>,
     next: Next,
 ) -> impl IntoResponse {
-
-    if !cfg!(debug_assertions) && bearer.is_none() {
-        /* require authentication in release mode */
-        tracing::warn!("No bearer token");
-        return StatusCode::UNAUTHORIZED.into_response();
-    }
-
-    if let Some(TypedHeader(Authorization(token))) = bearer {
-        if state.validate_visitor(token.token()).await.unwrap_or(false) {
+    tracing::warn!("Cookies: {cookie_jar:?}");
+    let uuid = cookie_jar.get(VISITOR_UUID_COOKIE_NAME);
+    if let Some(uuid) = uuid {
+        if state.validate_visitor(&uuid.value()).await.unwrap_or(false) {
             next.run(req).await
         } else {
             StatusCode::UNAUTHORIZED.into_response()
         }
     } else {
-        /* should not happen in release ever */
-        tracing::warn!("No bearer token, although debug mode");
-        next.run(req).await
+        (
+            StatusCode::EXPECTATION_FAILED,
+            Json(json!({"error": format!("No cookie with name={}", VISITOR_UUID_COOKIE_NAME)})),
+        )
+            .into_response()
     }
 }
 
@@ -73,6 +70,6 @@ pub fn api_router(state: AppState) -> axum::Router<AppState> {
         .nest("/projects", common::get_router())
         .layer(middleware::from_fn_with_state(
             state,
-            validate_visitor,
+            validate_visitor_cookie,
         ))
 }
